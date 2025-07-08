7634ca6c59f35f2ce17a208376be03cb2d764294
abaabaxxx <agt2643194651@outlook.com>
2025-07-08 16:42:38 +0800

    添加 git in competition 的教程

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/lby/tutorial_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/lby/tutorial_ws/devel;/home/lby/tianbot_ws/devel;/opt/ros/noetic'.split(';') if '/home/lby/tutorial_ws/devel;/home/lby/tianbot_ws/devel;/opt/ros/noetic' != '' else []
--- build/rqt_tutorial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lby/tutorial_ws/src/rqt_tutorial
-# Build directory: /home/lby/tutorial_ws/build/rqt_tutorial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
